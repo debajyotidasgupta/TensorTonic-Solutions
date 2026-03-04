1efcf63212742b38b8a3f1cd08f2f2cc46c29a69
Debajyoti Dasgupta <debajyotidasgupta6@gmail.com>
2026-03-04 15:59:23 +0100

    Update solution for vae-encoder

--- vae/vae-encoder/vae-encoder.py
+++ vae/vae-encoder/vae-encoder.py
@@ -5,13 +5,17 @@ def vae_encoder(x: np.ndarray, latent_dim: int) -> tuple:
     Encode input to latent distribution parameters.
     """
     B, input_dim = x.shape
+
+    W = np.random.randn(input_dim, latent_dim)
+    b = np.zeros((latent_dim,), dtype=x.dtype)
+    h = x @ W  + b
     
-    W_mu = np.random.randn(input_dim, latent_dim)
+    W_mu = np.random.randn(latent_dim, latent_dim)
     b_mu = np.zeros((latent_dim,), dtype=x.dtype)
-    mu = x @ W_mu + b_mu
+    mu = h @ W_mu + b_mu
     
-    W_logvar = np.random.randn(input_dim, latent_dim)
+    W_logvar = np.random.randn(latent_dim, latent_dim)
     b_logvar = np.zeros((latent_dim,), dtype=x.dtype)
-    logvar = x @ W_logvar + b_logvar
+    logvar = h @ W_logvar + b_logvar
 
     return mu, logvar
